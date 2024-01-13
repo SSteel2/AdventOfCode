bb3216d58ba1d8edffaff3ba405dc77682a706f0
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2024-01-13 15:25:07 +0200

    Refactored solution 14

--- 2023/14/14.py
+++ 2023/14/14.py
@@ -1,38 +1,58 @@
-import time
+import Util.input
+import Util.period
 
-input_lines = []
-with open('input.txt', 'r') as input_file:
-	for line in input_file:
-		input_lines.append(line.removesuffix('\n'))
+def getInput(filename):
+	return Util.input.LoadInput(Util.input.GetInputFile(__file__, filename))
 
-# Parse input
-chart = [[char for char in line] for line in input_lines]
-
-# Silver star
-def FindEmptyNorth(l, c):
-	result = l
-	for i in range(l - 1, -1, -1):
-		if chart[i][c] == '.':
+def _findEmpty(l, c, chart, direction):
+	check_range = {
+		'N': range(l - 1, -1, -1),
+		'S': range(l + 1, len(chart)),
+		'E': range(c + 1, len(chart[0])),
+		'W': range(c - 1, -1, -1)
+	}
+	if direction == 'N' or direction == 'S':
+		get_chart_value = lambda x: chart[x][c]
+		result = l
+	else:
+		get_chart_value = lambda x: chart[l][x]
+		result = c
+	for i in check_range[direction]:
+		if get_chart_value(i) == '.':
 			result = i
 		else:
 			break
 	return result
 
-def MoveNorth(l, c):
-	if l == 0:
+def _move(l, c, chart, direction):
+	condition = {
+		'N': l == 0,
+		'S': l == len(chart) - 1,
+		'E': c == len(chart[0]),
+		'W': c == 0
+	}
+	if condition[direction]:
 		return
-	new_line = FindEmptyNorth(l, c)
-	if new_line != l:
-		chart[new_line][c] = 'O'
-		chart[l][c] = '.'
+	new_param = _findEmpty(l, c, chart, direction)
+	chart[l][c] = '.'
+	if direction == 'N' or direction == 'S':
+		chart[new_param][c] = 'O'
+	else:
+		chart[l][new_param] = 'O'
 
-def MoveNorthAll():
-	for i, line in enumerate(chart):
-		for j, char in enumerate(line):
+def _moveAll(chart, direction):
+	enumeration_direction_line = 1
+	enumeration_direction_col = 1
+	if direction == 'S':
+		enumeration_direction_line = -1
+	if direction == 'E':
+		enumeration_direction_col = -1
+	for i, line in list(enumerate(chart))[::enumeration_direction_line]:
+		for j, char in list(enumerate(line))[::enumeration_direction_col]:
 			if char == 'O':
-				MoveNorth(i, j)
+				_move(i, j, chart, direction)	
 
-def CountScore():
+def _countScore(chart):
 	current_line_score = len(chart)
 	total_score = 0
 	for line in chart:
@@ -42,120 +62,23 @@ def CountScore():
 		current_line_score -= 1
 	return total_score
 
-MoveNorthAll()
+def silver(input_lines):
+	chart = [[char for char in line] for line in input_lines]
+	_moveAll(chart, 'N')
+	return _countScore(chart)
 
-with open('output_silver.txt', 'w') as write_file:
-	for line in chart:
-		write_file.write(''.join(line))
-		write_file.write('\n')
-
-
-
-print('Silver answer: ' + str(CountScore()))
-# Silver answer: 109466
-
-# Gold star
-chart = [[char for char in line] for line in input_lines]
-
-def FindEmptyWest(l, c):
-	result = c
-	for i in range(c - 1, -1, -1):
-		if chart[l][i] == '.':
-			result = i
-		else:
-			break
-	return result
-
-def MoveWest(l, c):
-	if c == 0:
-		return
-	new_col = FindEmptyWest(l, c)
-	if new_col != c:
-		chart[l][new_col] = 'O'
-		chart[l][c] = '.'
-
-def MoveWestAll():
-	for i, line in enumerate(chart):
-		for j, char in enumerate(line):
-			if char == 'O':
-				MoveWest(i, j)
-
-def FindEmptySouth(l, c):
-	result = l
-	for i in range(l + 1, len(chart)):
-		if chart[i][c] == '.':
-			result = i
-		else:
-			break
-	return result
-
-def MoveSouth(l, c):
-	if l == len(chart) - 1:
-		return
-	new_line = FindEmptySouth(l, c)
-	if new_line != l:
-		chart[new_line][c] = 'O'
-		chart[l][c] = '.'
-
-def MoveSouthAll():
-	for i, line in list(enumerate(chart))[::-1]:
-		for j, char in enumerate(line):
-			if char == 'O':
-				MoveSouth(i, j)
-
-def FindEmptyEast(l, c):
-	result = c
-	for i in range(c + 1, len(chart[0])):
-		if chart[l][i] == '.':
-			result = i
-		else:
-			break
-	return result
-
-def MoveEast(l, c):
-	if c == len(chart[0]):
-		return
-	new_col = FindEmptyEast(l, c)
-	if new_col != c:
-		chart[l][new_col] = 'O'
-		chart[l][c] = '.'
-	return chart
-
-def MoveEastAll():
-	for i, line in enumerate(chart):
-		for j, char in list(enumerate(line))[::-1]:
-			if char == 'O':
-				MoveEast(i, j)
-
-scores = {}
-scores_list = []
-start = time.time()
-for i in range(10000):
-	MoveNorthAll()
-	MoveWestAll()
-	MoveSouthAll()
-	MoveEastAll()
-	if i % 100 == 0:
-		print(i)
-	score = CountScore()
-	scores_list.append(score)
-	if score in scores:
-		scores[score] += 1
-	else:
-		scores[score] = 1
-end = time.time()
-
-with open('output.txt', 'w') as write_file:
-	for line in chart:
-		write_file.write(''.join(line))
-		write_file.write('\n')
-
-print('Gold answer: ' + str(CountScore()))
-
-print(f'Elasped time: {end - start:.2}')
-
-# best guess gold
-for i in sorted(list(scores.items()), key=lambda x: x[1], reverse=True):
-	print(f'{i[0]:4}: {i[1]}')
-
-print('Last 10 scores', scores_list[-10:])
+def gold(input_lines):
+	chart = [[char for char in line] for line in input_lines]
+	scores = []
+	elapsed_cycles = 368
+	period_calc_start = elapsed_cycles - 150
+	for i in range(elapsed_cycles):
+		_moveAll(chart, 'N')
+		_moveAll(chart, 'W')
+		_moveAll(chart, 'S')
+		_moveAll(chart, 'E')
+		scores.append(_countScore(chart))
+	period = Util.period.CalculatePeriod(scores[period_calc_start:])
+	target_cycles = 1000000000
+	remainder = (target_cycles - period_calc_start) % period
+	return scores[period_calc_start + remainder - 1]
