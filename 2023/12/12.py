3dbde15618cd10ced00b57c1997e1d32d537d064
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2024-01-13 13:39:57 +0200

    Refactored solution 13

--- 2023/12/12.py
+++ 2023/12/12.py
@@ -3,23 +3,20 @@ import Util.input
 def getInput(filename):
 	return Util.input.LoadInput(Util.input.GetInputFile(__file__, filename))
 
-def parse(line):
+def _parse(line):
 	return {'map': [char for char in line.split(' ')[0]], 'corrupt': [int(j) for j in line.split(' ')[1].split(',')]}
 
-def IsValid(current_map_string, corruptions):
-	return [len(i) for i in ''.join(current_map_string).split('.') if i != ''] == corruptions
-
-def IsCorruptionFit(current_map, start_index, corruption_length):
+def _isCorruptionFit(current_map, start_index, corruption_length):
 	if start_index + corruption_length > len(current_map):
 		return False
 	return all([i == '?' or i == '#' for i in current_map[start_index : start_index + corruption_length]])
 
-def FillCoruptSlot(current_map, start_index, corruptions, coruption_index, memoization):
+def _fillCoruptSlot(current_map, start_index, corruptions, coruption_index, memoization):
 	if (''.join(current_map[start_index:]), coruption_index) in memoization:
 		return memoization[(''.join(current_map[start_index:]), coruption_index)]
 	result = 0
 	for i in range(start_index, len(current_map)):
-		if (current_map[i] == '#' or current_map[i] == '?') and IsCorruptionFit(current_map, i, corruptions[coruption_index]):
+		if (current_map[i] == '#' or current_map[i] == '?') and _isCorruptionFit(current_map, i, corruptions[coruption_index]):
 			if coruption_index == len(corruptions) - 1:
 				if all([j == '.' or j == '?' for j in current_map[i + corruptions[coruption_index]:]]):
 					result += 1
@@ -30,23 +27,23 @@ def FillCoruptSlot(current_map, start_index, corruptions, coruption_index, memoi
 				for j in range(i, i + corruptions[coruption_index]):
 					next_map[j] = '#'
 				next_map[i + corruptions[coruption_index]] = '.'
-				result += FillCoruptSlot(next_map, i + corruptions[coruption_index] + 1, corruptions, coruption_index + 1, memoization)
+				result += _fillCoruptSlot(next_map, i + corruptions[coruption_index] + 1, corruptions, coruption_index + 1, memoization)
 		if current_map[i] == '#':
 			break
 	memoization[(''.join(current_map[start_index:]), coruption_index)] = result
 	return result
 
-def calculateCorruption(springs):
+def _calculateCorruption(springs):
 	grand_sum = 0
 	for spring in springs:
-		grand_sum += FillCoruptSlot(spring['map'], 0, spring['corrupt'], 0, {})
+		grand_sum += _fillCoruptSlot(spring['map'], 0, spring['corrupt'], 0, {})
 	return grand_sum	
 
 def silver(input_lines):
-	springs = Util.input.ParseInputLines(input_lines, parse)
-	return calculateCorruption(springs)
+	springs = Util.input.ParseInputLines(input_lines, _parse)
+	return _calculateCorruption(springs)
 
 def gold(input_lines):
-	springs = Util.input.ParseInputLines(input_lines, parse)
+	springs = Util.input.ParseInputLines(input_lines, _parse)
 	springs = [{'map': (i['map'] + ['?']) * 4 + i['map'], 'corrupt': i['corrupt'] * 5} for i in springs]
-	return calculateCorruption(springs)
+	return _calculateCorruption(springs)
