581058c4701ab475c0cb7302f978426022fa6425
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2024-01-24 19:44:57 +0200

    Added time taken reporting to main class

--- aoc.py
+++ aoc.py
@@ -3,6 +3,13 @@ import importlib.util
 # I could have used importlib.import_module, but the catch is to use dots '.' instead of slashes...
 import argparse
 import os
+import time
+
+def timedExecution(func, input_lines):
+	start_time = time.perf_counter_ns()
+	result = func(input_lines)
+	end_time = time.perf_counter_ns()
+	return result, end_time - start_time
 
 def runSolution(year, day):
 	day_number = int(day)
@@ -13,15 +20,31 @@ def runSolution(year, day):
 	spec.loader.exec_module(module)
 
 	input_lines = module.getInput('input.txt')
-	result_silver = module.silver(input_lines)
-	print('Silver answer: ' + str(result_silver))
-	result_gold = module.gold(input_lines)
-	print('Gold answer: ' + str(result_gold))
+	result_silver, time_silver = timedExecution(module.silver, input_lines)
+	result_gold, time_gold = timedExecution(module.gold, input_lines)
+	return (result_silver, result_gold), (time_silver, time_gold)
 
+def printAnswers(results):
+	print(f'[Answer]  Silver {results[0]:18}  | Gold: {results[1]:18}')
 
-if __name__ == '__main__':
+def printTime(times):
+	print(f'[Time (s)]  Silver {times[0] // 100000 / 10000:15.4f}  | Gold: {times[1] // 100000 / 10000:15.4f}')
+
+def main():
 	parser = argparse.ArgumentParser(prog='Advent of Code', description='Advent of Code solution runner')
 	parser.add_argument('year', help='2-digit year number')
 	parser.add_argument('day', help='day number')
+	parser.add_argument('-t', '--time', action='store_true', help='output time taken')
+	parser.add_argument('-a', '--answers', action='store_false', help='do not output answers (default=True)', default=True)
 	args = parser.parse_args()
-	runSolution(args.year, args.day)
+	if not args.time and not args.answers:
+		print("At least one output mode must be selected. -a and -t options are both false.")
+		return
+	results, times = runSolution(args.year, args.day)
+	if args.answers:
+		printAnswers(results)
+	if args.time:
+		printTime(times)
+
+if __name__ == '__main__':
+	main()
