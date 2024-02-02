359f48e59b23cf2cad58070f2f170a27ef8d24d6
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2024-02-02 16:49:37 +0200

    Optimized 2023 day 11

--- 2023/11/11.py
+++ 2023/11/11.py
@@ -3,42 +3,52 @@ import Util.input
 def getInput(filename):
 	return Util.input.LoadInput(Util.input.GetInputFile(__file__, filename))
 
-def _distance(a, b, empty_rows, empty_cols, multiplier):
-	min_rows = min(a[0], b[0])
-	max_rows = max(a[0], b[0])
-	min_cols = min(a[1], b[1])
-	max_cols = max(a[1], b[1])
-
-	distance = max_rows - min_rows + max_cols - min_cols
-	longs = empty_rows[max_rows] - empty_rows[min_rows] + empty_cols[max_cols] - empty_cols[min_cols]
+def _distance1D(a, b, empty_locations, multiplier):
+	small = min(a, b)
+	big = max(a, b)
+	distance = big - small
+	longs = empty_locations[big] - empty_locations[small]
 	return longs * multiplier + distance
 
 def _findStarDistances(galaxy, expansion_multiplier):
+	stars_rows = {}
+	stars_cols = {}
+	for i, line in enumerate(galaxy):
+		for j, val in enumerate(line):
+			if val == '#':
+				if i not in stars_rows:
+					stars_rows[i] = 1
+				else:
+					stars_rows[i] += 1
+				if j not in stars_cols:
+					stars_cols[j] = 1
+				else:
+					stars_cols[j] += 1
+	stars_rows_keys = list(stars_rows.keys())
+	stars_cols_keys = list(stars_cols.keys())
+
 	empty_cols = []
 	current_value = 0
 	for i in range(len(galaxy[0])):
-		if all(galaxy[j][i] == '.' for j in range(len(galaxy[0]))):
+		if i not in stars_cols_keys:
 			current_value += 1
 		empty_cols.append(current_value)
 
 	empty_rows = []
 	current_value = 0
 	for i, values in enumerate(galaxy):
-		if all(j == '.' for j in values):
+		if i not in stars_rows_keys:
 			current_value += 1
 		empty_rows.append(current_value)
 
-	stars = []
-	for i, line in enumerate(galaxy):
-		for j, val in enumerate(line):
-			if val == '#':
-				stars.append((i, j))
-
 	distance_sum = 0
 	multiplier = expansion_multiplier - 1
-	for i in range(len(stars)):
-		for j in range(i + 1, len(stars)):
-			distance_sum += _distance(stars[i], stars[j], empty_rows, empty_cols, multiplier)
+	for i in range(len(stars_rows_keys)):
+		for j in range(i + 1, len(stars_rows_keys)):
+			distance_sum += (_distance1D(stars_rows_keys[i], stars_rows_keys[j], empty_rows, multiplier) * stars_rows[stars_rows_keys[i]] * stars_rows[stars_rows_keys[j]])
+	for i in range(len(stars_cols_keys)):
+		for j in range(i + 1, len(stars_cols_keys)):
+			distance_sum += (_distance1D(stars_cols_keys[i], stars_cols_keys[j], empty_cols, multiplier) * stars_cols[stars_cols_keys[i]] * stars_cols[stars_cols_keys[j]])
 	return distance_sum
 
 def silver(input_lines):
