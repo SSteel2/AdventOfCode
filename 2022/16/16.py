cc57a1f27389802f0fb41a0b41b5a0e0cdc5861d
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2024-02-15 18:19:51 +0200

    Year 2022 solution 16 finalized and refactored

--- 2022/16/16.py
+++ 2022/16/16.py
@@ -29,66 +29,94 @@ def _bfs(rooms, room):
 			queue.append(neighbour)
 	return distances
 
+def _createTraversalRoutes(room, room_names, distances):
+	routes = {}
+	for i in room_names:
+		routes[i] = []
+		if room == i:
+			continue
+		for traverse_room in room_names:
+			if traverse_room == i or traverse_room == room:
+				continue
+			if distances[room][i] == distances[room][traverse_room] + distances[traverse_room][i]:
+				routes[i].append(traverse_room)
+	return routes
+
 def _createDenseGraph(rooms):
-	goal_rooms = []
-	for i in rooms:
-		if rooms[i]['rate'] > 0:
-			goal_rooms.append(i)
-	# traversal_graph = [[0 for _ in goal_rooms] for _ in goal_rooms]
+	goal_rooms = [i for i in rooms if rooms[i]['rate'] > 0]
+	goal_rooms.append('AA')
 	traversal_graph = {}
-	for i, room_i in enumerate(goal_rooms):
-		distances = _bfs(rooms, room_i)
-		traversal_graph[room_i] = distances
-		# for j, room_j in enumerate(goal_rooms):
-		# 	if room_i == room_j:
-		# 		continue
-		# 	# traversal_graph[i][j] = distances[room_j]
-	return goal_rooms, traversal_graph
+	for i in goal_rooms:
+		distances = _bfs(rooms, i)
+		traversal_graph[i] = {}
+		for j in goal_rooms:
+			traversal_graph[i][j] = distances[j]
+		traversal_graph[i][i] = 0
 
-def silver(input_lines):
-	rooms = _parse(input_lines)
-	names, distances = _createDenseGraph(rooms)
+	traversal_routes = {}
+	for room in goal_rooms:
+		traversal_routes[room] = _createTraversalRoutes(room, goal_rooms, traversal_graph)
+	return goal_rooms, traversal_graph, traversal_routes
 
-	# print('  ' + ''.join([str(j).rjust(4, ' ') for j in names]))
-	# for i in range(len(names)):
-	# 	row = ''.join([str(j).rjust(4, ' ') for j in distances[i]])
-	# 	print(f'{names[i]}{row}')
+def _isPathEfficient(start_node, end_node, value_gain, routes, visited, distances, rooms, max_time):
+	for route in routes[start_node][end_node]:
+		if route in visited:
+			continue
+		possible_value = (max_time - distances[start_node][route] - 1) * rooms[route]['rate'] + \
+			(max_time - distances[start_node][route] - 1 - distances[route][end_node] - 1) * rooms[end_node]['rate']
+		if possible_value > value_gain:
+			return False
+	return True
 
-	start_distances = _bfs(rooms, 'AA')
-	# print(start_distances)
-
+def _walk(max_time, names, distances, routes, rooms, is_return_positions=False, disabled_nodes=[]):
 	queue = Util.PriorityQueue.PriorityQueue()
-	for name in names:
-		queue.append(start_distances[name] + 1, {'distance': start_distances[name] + 1, 'visited': set([name]), 'value': (29 - start_distances[name]) * rooms[name]['rate'], 'path': [name]})
+	start_node = 'AA'
+	queue.append(0, {'distance': 0, 'value': 0, 'path': disabled_nodes + [start_node]})
 
 	max_value = 0
-	max_path = []
+	end_positions = []
 	while len(queue) > 0:
 		current = queue.pop()
-		# print(current)
+		is_moving = False
 		for name in names:
-			if name in current['visited']:
+			if name in current['path'] or name == start_node:
 				continue
-			# print(distances)
-			# print(current['path'][-1])
 			new_distance = current['distance'] + distances[current['path'][-1]][name] + 1
-			if new_distance > 30:
+			if new_distance > max_time:
 				continue
-			new_value = current['value'] + (30 - new_distance) * rooms[name]['rate']
+
+			value_gain = (max_time - new_distance) * rooms[name]['rate']
+			if not _isPathEfficient(current['path'][-1], name, value_gain, routes, current['path'], distances, rooms, max_time):
+				continue
+
+			new_value = current['value'] + value_gain
 			new_path = current['path'] + [name]
 			if new_value > max_value:
 				max_value = new_value
-				max_path = new_path
-			new_visited = current['visited'].copy()
-			new_visited.add(name)
-			current_position = {'distance': new_distance, 'visited': new_visited, 'value': new_value, 'path': new_path}
+			current_position = {'distance': new_distance, 'value': new_value, 'path': new_path}
 
 			queue.append(new_distance, current_position)
-	# print(max_path)
-	return max_value
+			is_moving = True
+		if is_return_positions and not is_moving:
+			end_positions.append(current)
+	return end_positions if is_return_positions else max_value
 
-	# for i in rooms:
-	# 	print(i, rooms[i])
+def silver(input_lines):
+	rooms = _parse(input_lines)
+	names, distances, routes = _createDenseGraph(rooms)
+	max_time = 30
+	return _walk(max_time, names, distances, routes, rooms)
 
 def gold(input_lines):
-	pass
+	rooms = _parse(input_lines)
+	names, distances, routes = _createDenseGraph(rooms)
+	max_time = 26
+	end_positions = _walk(max_time, names, distances, routes, rooms, True)
+
+	global_max_value = 0
+	for end_position in end_positions:
+		local_max_value = _walk(max_time, names, distances, routes, rooms, disabled_nodes=end_position['path'])
+		if local_max_value + end_position['value'] > global_max_value:
+			global_max_value = local_max_value + end_position['value']
+
+	return global_max_value
