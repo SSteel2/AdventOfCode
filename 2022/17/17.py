23ae9869f4a95774ca7bcff8170f8571778a4d58
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2024-02-18 20:36:28 +0200

    Year 2022 solution 17 gold added

--- 2022/17/17.py
+++ 2022/17/17.py
@@ -1,4 +1,5 @@
 import Util.input
+import Util.period
 
 def getInput(filename):
 	return Util.input.LoadInput(Util.input.GetInputFile(__file__, filename))
@@ -10,6 +11,8 @@ Shapes = [
 	[['#'], ['#'], ['#'], ['#']],
 	[['#', '#'], ['#', '#']]
 ]
+
+BoardWidth = 7
 
 def _findHighestRock(field):
 	for i, line in enumerate(field[::-1]):
@@ -27,7 +30,7 @@ def _move(position, instruction):
 
 def _isValidMove(shape, position, instruction, field):
 	if instruction == '>':
-		if position[1] + len(shape[0]) >= len(field[0]):
+		if position[1] + len(shape[0]) >= BoardWidth:
 			return False
 		new_position = (position[0], position[1] + 1)
 	elif instruction == '<':
@@ -59,58 +62,84 @@ def _towerHeight(field):
 		empty_rows += 1
 	return len(field) - empty_rows
 
-def _debugPrintField(field):
-	for i, line in enumerate(field[::-1]):
-		print(f'{len(field) - i - 1:>3} {"".join(line)}')
+def _dropShape(shape, field, instructions, instruction_number):
+	sapwn_vertical = _towerHeight(field) + 3
+	shape_height = len(shape)
+	shape_width = len(shape[0])
+	height_needed = sapwn_vertical + shape_height - len(field)
+	for _ in range(height_needed):
+		field.append(['.' for _ in range(BoardWidth)])
+	current_position = (sapwn_vertical, 2)
+	is_moving = True
+	lines_dropped = 0
+	vertical_move = 0
+	while is_moving:
+		instruction = instructions[instruction_number % len(instructions)]
+		if lines_dropped < 3:
+			if instruction == '>':
+				if current_position[1] + vertical_move + shape_width < BoardWidth:
+					vertical_move += 1
+			elif instruction == '<':
+				if current_position[1] + vertical_move > 0:
+					vertical_move -= 1
+		elif lines_dropped == 3:
+			current_position = (current_position[0] - 3, current_position[1] + vertical_move)
+		if lines_dropped >= 3:
+			if _isValidMove(shape, current_position, instruction, field):
+				current_position = _move(current_position, instruction)
+			if _isValidMove(shape, current_position, 'v', field):
+				current_position = _move(current_position, 'v')
+			else:
+				is_moving = False
+				_placeShape(shape, current_position, field)
+
+		lines_dropped += 1
+		instruction_number += 1
+	return instruction_number
+
 
 def silver(input_lines):
-	board_width = 7
 	instructions = input_lines[0]
 	instruction_number = 0
-	field = [['.' for _ in range(board_width)] for _ in range(5)]
+	field = [['.' for _ in range(BoardWidth)] for _ in range(5)]
 	shape_count = 0
 	total_shapes = 2022
 	while shape_count < total_shapes:
-		sapwn_vertical = _findHighestRock(field) + 4
-		if shape_count == 0:
-			sapwn_vertical = 3
 		shape = Shapes[shape_count % len(Shapes)]
-		shape_height = len(shape)
-		shape_width = len(shape[0])
-		height_needed = sapwn_vertical + shape_height - len(field)
-		for _ in range(height_needed):
-			field.append(['.' for _ in range(board_width)])
-		current_position = (sapwn_vertical, 2)
-		is_moving = True
-		lines_dropped = 0
-		vertical_move = 0
-		while is_moving:
-			instruction = instructions[instruction_number % len(instructions)]
-			if lines_dropped < 3:
-				if instruction == '>':
-					if current_position[1] + vertical_move + shape_width < board_width:
-						vertical_move += 1
-				elif instruction == '<':
-					if current_position[1] + vertical_move > 0:
-						vertical_move -= 1
-			elif lines_dropped == 3:
-				current_position = (current_position[0] - 3, current_position[1] + vertical_move)
-			if lines_dropped >= 3:
-				if _isValidMove(shape, current_position, instruction, field):
-					current_position = _move(current_position, instruction)
-				if _isValidMove(shape, current_position, 'v', field):
-					current_position = _move(current_position, 'v')
-				else:
-					is_moving = False
-					_placeShape(shape, current_position, field)
-
-			lines_dropped += 1
-			instruction_number += 1
-
+		instruction_number = _dropShape(shape, field, instructions, instruction_number)
 		shape_count += 1
-
-	# _debugPrintField(field)
 	return _towerHeight(field)
 
 def gold(input_lines):
-	pass
+	instructions = input_lines[0]
+	instruction_length = len(instructions)
+	instruction_number = 0
+	field = [['.' for _ in range(BoardWidth)] for _ in range(5)]
+	shape_count = 0
+	target_shapes = 1000000000000
+	history_instruction_remainder = []
+	history_tower_height = [0]
+	initial_slack = 1200
+	period_counts = 0
+	
+	while instruction_number < instruction_length * 10 or instruction_number < 10000:
+		shape = Shapes[shape_count % len(Shapes)]
+		instruction_number = _dropShape(shape, field, instructions, instruction_number)
+		shape_count += 1
+		if shape_count % 5 == 0:
+			current_height = _towerHeight(field)
+			current_instruction_remainder = instruction_number % instruction_length
+			history_instruction_remainder.append(current_instruction_remainder)
+			history_tower_height.append(current_height)
+			if instruction_number > instruction_length * 5 // 3 and shape_count % 100 == 0:
+				initial_slack += 10
+				period = Util.period.CalculatePeriod(history_instruction_remainder[initial_slack // 5:])
+				period_counts += 1
+				if period != None:
+					break
+
+	increments = (target_shapes - initial_slack) // (period * 5)
+	increment_height = history_tower_height[-1] - history_tower_height[-1 - period]
+	target_height = history_tower_height[(initial_slack + (target_shapes - initial_slack) % (period * 5)) // 5] + increments * increment_height
+
+	return target_height
