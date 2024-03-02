f1665315fa77b9616657153ccbacdc17bc6242a0
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2024-03-02 02:34:37 +0200

    Year 2022 solution 20 made into linked lists with highway

--- 2022/20/20.py
+++ 2022/20/20.py
@@ -9,32 +9,68 @@ def _parse(line):
 def _parseGold(line):
 	return int(line) * 811589153
 
+class HighwayNode:
+	def __init__(self, node):
+		self.node = node
+		self.next_node = None
+		self.prev_node = None
+		self.jump_size = None
+
+	def __str__(self):
+		return f'HN {self.node.value}'
+
+	def __repr__(self):
+		return self.__str__()
+
+	def pushForward(self, end_node):
+		current = self.node
+		current.highway_node = None
+		current.next_node.highway_node = self
+		self.node = current.next_node
+		if self != end_node:
+			self.next_node.pushForward(end_node)
+
 class Node:
 	def __init__(self, index, value):
 		self.index = index
 		self.value = value
 		self.next_node = None
 		self.prev_node = None
+		self.highway_node = None
 
 	def __str__(self):
-		return f'{value}'
+		return f'{self.value}'
 
 	def __repr__(self):
 		return self.__str__()
 
 	def get_node(self, move):
-		if move == 0:
-			return self
-		elif move > 0:
-			current = self
-			for i in range(move):
+		current = self
+		moved = 0
+		start_highway = None
+		last_highway = None
+		while moved < move:
+			if current.highway_node != None:
+				last_highway = current.highway_node
+				if start_highway == None:
+					if moved == 0:
+						start_highway = current.highway_node
+					else:
+						start_highway = current.highway_node.prev_node
+				if move - moved >= last_highway.jump_size:
+					moved += last_highway.jump_size
+					current = last_highway.next_node.node
+				else:
+					current = current.next_node
+					moved += 1
+			else:
 				current = current.next_node
-			return current
-		else:
-			current = self.prev_node
-			for i in range(0, move, -1):
-				current = current.prev_node
-			return current
+				moved += 1
+		if current.highway_node != None:
+			last_highway = current.highway_node
+			if start_highway == None:
+				start_highway = current.highway_node.prev_node
+		return current, start_highway, last_highway
 
 class LinkedList:
 	def __init__(self):
@@ -68,82 +104,64 @@ class LinkedList:
 		self.nodes[self.length].prev_node.next_node = self.nodes[self.length]
 		self.length += 1
 
+	def createHighwayNodes(self, jump_size):
+		first_highway = self.nodes[0].highway_node = HighwayNode(self.nodes[0])
+		last_highway = first_highway
+		current = self.nodes[0].next_node
+		traveled = 0
+		while first_highway != current:
+			traveled += 1
+			if traveled % jump_size == 0:
+				current_highway = current.highway_node = HighwayNode(current)
+				last_highway.next_node = current_highway
+				last_highway.jump_size = jump_size
+				current_highway.prev_node = last_highway
+				last_highway = current_highway
+				if self.length - traveled < 2 * jump_size:
+					break
+			current = current.next_node
+		last_highway.next_node = first_highway
+		last_highway.jump_size = self.length - traveled
+		first_highway.prev_node = last_highway		
+
 	def reorder(self):
 		for i in range(self.length):
 			node = self.nodes[i]
 			move = node.value
-			if move == 0:
+			if move % (self.length - 1) == 0:
 				continue
 			old_next = node.next_node
 			old_prev = node.prev_node
-			new_prev = node.get_node(move % (self.length - 1))
+			new_prev, old_highway, new_highway = node.get_node(move % (self.length - 1))
 			old_prev.next_node = old_next
 			old_next.prev_node = old_prev
 			node.prev_node = new_prev
 			node.next_node = new_prev.next_node
 			new_prev.next_node.prev_node = node
 			new_prev.next_node = node
-			# print(self)
+			if node.highway_node != None:
+				old_next.highway_node = node.highway_node
+				node.highway_node = None
+				old_next.highway_node.node = old_next
+			if old_highway != None and new_highway != None and old_highway != new_highway:
+				old_highway.next_node.pushForward(new_highway)
 
 	def sum_criticals(self):
 		critical_indexes = [1000, 2000, 3000]
-		# critical_indexes = [i % self.length for i in critical_indexes]
 		total = 0
 		for i in critical_indexes:
-			v = self.nodes[self.start_index].get_node(i % self.length).value
-			total += v
-
-			print(f'{i % self.length}: {v}')
-			# print(total)
+			node, _, _ = self.nodes[self.start_index].get_node(i % self.length)
+			total += node.value
 		return total
 
-	def debug(self):
-		current = self.nodes[0]
-		for i in range(self.length):
-			print(f'Node {current.index}: {current.value} (next: {current.next_node.value}, prev: {current.prev_node.value})')
-			current = current.next_node
-
-def _reorder(indexed_list):
-	for i in range(len(indexed_list)):
-		for j, index_value in enumerate(indexed_list):
-			if index_value[0] == i:
-				moves = index_value[1]
-				new_index = (j + moves) % (len(indexed_list) - 1)
-				indexed_value = indexed_list.pop(j)
-				indexed_list.insert(new_index, indexed_value)
-				break
-	return indexed_list
-
-def _findZeroIndex(indexed_list):
-	for i, index_value in enumerate(indexed_list):
-		if index_value[1] == 0:
-			return i
-
-def _calculateCriticalSum(indexed_list):
-	zero_index = _findZeroIndex(indexed_list)
-	critical_indexes = [1000, 2000, 3000]
-	total = 0
-	for i in critical_indexes:
-		total += indexed_list[(i + zero_index) % len(indexed_list)][1]
-	return total
-
-def _failedSolution():
-	parsed = Util.input.ParseInputLines(input_lines, _parse)
+def _solve(parsed, iterations):
 	linked_list = LinkedList()
 	for i in parsed:
 		linked_list.add(i)
-	linked_list.reorder()
-	print(linked_list)
-	linked_list.debug()
-	return linked_list.sum_criticals()
-
-def _solve(numbers, iterations):
-	indexed_list = []
-	for i, number in enumerate(numbers):
-		indexed_list.append((i, number))
+	linked_list.createHighwayNodes(120)
 	for i in range(iterations):
-		indexed_list = _reorder(indexed_list)
-	return _calculateCriticalSum(indexed_list)	
+		linked_list.reorder()
+	return linked_list.sum_criticals()
 
 def silver(input_lines):
 	parsed = Util.input.ParseInputLines(input_lines, _parse)
