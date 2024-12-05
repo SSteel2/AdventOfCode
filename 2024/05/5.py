92b17b27fe17ac367aefc125a4d6a3b270f210bc
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2024-12-05 16:38:25 +0200

    Year 2024 solution 5 optimized. Added quick select algorithm implementation for future use cases.

--- 2024/05/5.py
+++ 2024/05/5.py
@@ -1,4 +1,5 @@
 import Util.input
+from functools import cmp_to_key
 
 def getInput(filename):
 	return Util.input.LoadInput(Util.input.GetInputFile(__file__, filename))
@@ -30,16 +31,34 @@ def _check_rule_adhearing(update, rules):
 				return False
 	return True
 
+def _compare_order(a, b, rules):
+	if a in rules and b in rules[a]:
+		return -1
+	if b in rules and a in rules[b]:
+		return 1
+	return 0
+
 def _make_rule_adhearing(update, rules):
-	current_order = update
-	while not _check_rule_adhearing(current_order, rules):
-		for index, page in enumerate(current_order):
-			if page not in rules:
-				continue
-			for pre_index, pre_page in enumerate(current_order[:index]):
-				if pre_page in rules[page]:
-					current_order = current_order[:pre_index] + [current_order[index]] + current_order[pre_index:index] + current_order[index + 1:]
-	return current_order
+	return sorted(update, key=cmp_to_key(lambda a, b: _compare_order(a, b, rules)))
+
+def _quickselect(pages, index, rules):
+	'''This is a really cool O(n) median selection algorithm. It actually selects the k-th largest element in a list.
+	More information about it: https://rcoh.me/posts/linear-time-median-finding/
+	Unfortunately, since the lists are really small a standard sort approach works ~0.0038s faster in this case.
+	Leaving this in here in case I ever need something like it.
+	'''
+	if len(pages) == 1:
+		return pages[0]
+	pivot = pages[0]
+	lows = [page for page in pages if _compare_order(page, pivot, rules) == -1]
+	highs = [page for page in pages if _compare_order(page, pivot, rules) == 1]
+	pivots = [page for page in pages if _compare_order(page, pivot, rules) == 0]
+	if index < len(lows):
+		return _quickselect(lows, index, rules)
+	elif index < len(lows) + len(pivots):
+		return pivots[0]
+	else:
+		return _quickselect(highs, index - len(lows) - len(pivots), rules)
 
 def silver(input_lines):
 	rules, updates = _parse(input_lines)
@@ -56,4 +75,5 @@ def gold(input_lines):
 		if not _check_rule_adhearing(update, rules):
 			correct_order = _make_rule_adhearing(update, rules)
 			middle_sum += correct_order[len(correct_order) // 2]
+			# middle_sum += _quickselect(update, len(update) // 2, rules)
 	return middle_sum
