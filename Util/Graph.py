98bd876839450515cc9ef786cb0be13d24b4cb78
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2025-01-17 02:14:16 +0200

    Added Util.Graph class

---
+++ Util/Graph.py
@@ -0,0 +1,30 @@
+class Graph:
+	def __init__(self):
+		self.nodes = {}  # name/id: Node
+		self.edges = []
+
+	def AddNode(self, name):
+		if name not in self.nodes:
+			self.nodes[name] = Node(name)
+		return self.nodes[name]
+
+	def AddEdge(self, left, right, weight):
+		edge = Edge(left, right, weight)
+		self.edges.append(edge)
+		left.AddEdge(edge)
+		right.AddEdge(edge)
+
+class Node:
+	def __init__(self, name):
+		self.name = name
+		self.edges = {}
+
+	def AddEdge(self, edge):
+		other_node = edge.left if edge.right.name == self.name else edge.right
+		self.edges[other_node.name] = edge
+
+class Edge:
+	def __init__(self, left, right, weight):
+		self.left = left
+		self.right = right
+		self.weight = weight
