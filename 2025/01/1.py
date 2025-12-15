b73014c35276c151a03292d05d7ed70926b44dd9
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2025-12-16 00:12:21 +0200

    Year 2025 solution 1 refactored

--- 2025/01/1.py
+++ 2025/01/1.py
@@ -6,26 +6,30 @@ def getInput(filename):
 def _parse(line):
 	return int(line[1:]) if line[0] == 'R' else -int(line[1:])
 
-def silver(input_lines):
-	numbers = Util.input.ParseInputLines(input_lines, _parse)
-	result = 0
-	pointer = 50
-	for number in numbers:
-		pointer = (pointer + number) % 100
-		if pointer == 0:
-			result += 1
-	return result
-
-def gold(input_lines):
-	numbers = Util.input.ParseInputLines(input_lines, _parse)
+def _solve(numbers, score_func):
 	result = 0
 	pointer = 50
 	for number in numbers:
 		if pointer == 0 and number < 0:
-			result -= 1
+			pointer += 100
 		pointer += number
-		result += abs(pointer // 100)
-		if pointer % 100 == 0 and pointer <= 0:
-			result += 1
+		result += score_func(pointer)
 		pointer %= 100
 	return result
+
+def _silver_score(pointer):
+	return 1 if pointer % 100 == 0 else 0
+
+def _gold_score(pointer):
+	result = abs(pointer // 100)
+	if pointer % 100 == 0 and pointer <= 0:
+		result += 1
+	return result
+
+def silver(input_lines):
+	numbers = Util.input.ParseInputLines(input_lines, _parse)
+	return _solve(numbers, _silver_score)
+
+def gold(input_lines):
+	numbers = Util.input.ParseInputLines(input_lines, _parse)
+	return _solve(numbers, _gold_score)
