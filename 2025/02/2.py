f67b90f76970f50e8bf3b142493ef7bc1d292f63
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2025-12-16 00:45:24 +0200

    Year 2025 solution 2 refactored

--- 2025/02/2.py
+++ 2025/02/2.py
@@ -30,41 +30,34 @@ def _construct_multiple(number, helper, repetitions):
 	return result
 
 def _repetition_sum(start, end, digits, repetitions, used_ids):
-	# print(start, end, digits, repetitions)
 	result = 0
 	helper = 10 ** (digits // repetitions)
 	start_sequence = start // (helper ** (repetitions - 1))
 	end_sequence = end // (helper ** (repetitions - 1))
-	# print(start_sequence, end_sequence)
 	for number in range(start_sequence, end_sequence + 1):
 		current_id = _construct_multiple(number, helper, repetitions)
 		if current_id < start or current_id in used_ids:
 			continue
 		if current_id > end:
 			break
-		# print(start, end, digits, repetitions, current_id)
 		result += current_id
 		used_ids.add(current_id)
 	return result
 
-def silver(input_lines):
-	ranges = _normalize_ranges(_parse(input_lines[0]))
+def _solve(ranges, max_repetitions):
 	total_result = 0
 	for i in ranges:
 		digits = len(str(i[0]))
 		used_ids = set()
-		if digits % 2 == 0:
-			total_result += _repetition_sum(i[0], i[1], digits, 2, used_ids)
-	return total_result
-	# pass
+		for repetitions in range(2, max_repetitions + 1):
+			if digits % repetitions == 0:
+				total_result += _repetition_sum(i[0], i[1], digits, repetitions, used_ids)
+	return total_result	
+
+def silver(input_lines):
+	ranges = _normalize_ranges(_parse(input_lines[0]))
+	return _solve(ranges, 2)
 
 def gold(input_lines):
 	ranges = _normalize_ranges(_parse(input_lines[0]))
-	total_result = 0
-	for i in ranges:
-		digits = len(str(i[0]))
-		used_ids = set()
-		for repetitions in range(2, 10):
-			if digits % repetitions == 0:
-				total_result += _repetition_sum(i[0], i[1], digits, repetitions, used_ids)
-	return total_result
+	return _solve(ranges, 10)
