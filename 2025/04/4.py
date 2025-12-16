fc11b56e7ed24e7ee1b9a1811fce98121a9c737c
Vytautas Valiukonis <vytautas.valiukonis@outlook.com>
2025-12-16 23:12:44 +0200

    Year 2025 solution 4 refactored

--- 2025/04/4.py
+++ 2025/04/4.py
@@ -1,42 +1,46 @@
 import Util.input
 import Util.directions
-import copy
 
 def getInput(filename):
 	return Util.input.LoadInput(Util.input.GetInputFile(__file__, filename))
 
 def silver(input_lines):
 	padded_table = Util.directions.PadTable(input_lines, 1, '.')
-	moveable_paper = 0
-	for l, line in enumerate(padded_table):
+	_, paper_queue = _construct_neighbour_table(padded_table)
+	return len(paper_queue)
+
+def _construct_neighbour_table(table):
+	current_table = [[-1 for _ in range(len(table[0]))] for _ in table]
+	paper_queue = []
+	for l, line in enumerate(table):
 		for c, col in enumerate(line):
 			if col == '@':
 				adjacent_count = 0
 				for d in Util.directions.DirectionsTableDiagonals:
-					if Util.directions.Get(padded_table, Util.directions.Move((l, c), d)) == '@':
+					if Util.directions.Get(table, Util.directions.Move((l, c), d)) == '@':
 						adjacent_count += 1
+				Util.directions.Set(current_table, (l, c), adjacent_count)
 				if adjacent_count < 4:
-					moveable_paper += 1
-	return moveable_paper
+					paper_queue.append((l, c))
+	return current_table, paper_queue
 
 def gold(input_lines):
-	# padaryti, kad tikrintų, tik šalia prieš
-	current_table = Util.directions.PadTable(input_lines, 1, '.')
+	padded_table = Util.directions.PadTable(input_lines, 1, -1)
+	current_table, paper_queue = _construct_neighbour_table(padded_table)
+
 	moveable_paper = 0
-	last_iteration_paper = -1
-	while last_iteration_paper < moveable_paper:
-		# patikrinti, ar čia greičiau už list comprehension
-		next_table = copy.deepcopy(current_table)
-		last_iteration_paper = moveable_paper
-		for l, line in enumerate(current_table):
-			for c, col in enumerate(line):
-				if col == '@':
-					adjacent_count = 0
-					for d in Util.directions.DirectionsTableDiagonals:
-						if Util.directions.Get(current_table, Util.directions.Move((l, c), d)) == '@':
-							adjacent_count += 1
-					if adjacent_count < 4:
-						Util.directions.Set(next_table, (l, c), '.')
-						moveable_paper += 1
-		current_table = next_table
+	while len(paper_queue) > 0:
+		current_cell = paper_queue.pop()
+		if Util.directions.Get(current_table, current_cell) == -1:
+			continue
+		Util.directions.Set(current_table, current_cell, -1)
+		for d in Util.directions.DirectionsTableDiagonals:
+			neighbour = Util.directions.Move(current_cell, d)
+			neighbour_value = Util.directions.Get(current_table, neighbour)
+			if neighbour_value == -1:
+				continue
+			Util.directions.Set(current_table, neighbour, neighbour_value - 1)
+			if neighbour_value - 1 < 4:
+				paper_queue.append(neighbour)
+		moveable_paper += 1
 	return moveable_paper
